15bce1896985d8c35c02ea677093d5c6968b712f
xxKiiRa <saifbrilliantoro@gmail.com>
2026-08-01 18:19:53 +0700

    Persiapan deploy Hostinger

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,4 +1,3 @@
-
 <?php
 
 namespace Tests\Feature;
@@ -11,10 +10,10 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_guests_are_redirected_to_the_login_page(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect('/login');
     }
 }
